6958946b10e7b97307b283d05874721c4f6d39e1
Jacob Herper <jacob@herper.uk>
2019-01-20 14:55:07 +0000

    Controls fixed

--- src/components/Burger/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControl/BuildControl.js
@@ -5,8 +5,16 @@ import classes from "./BuildControl.css";
 const buildControl = props => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less}>-</button>
-    <button className={classes.More}>+</button>
+    <button
+      className={classes.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      -
+    </button>
+    <button className={classes.More} onClick={props.added}>
+      +
+    </button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -24,9 +24,21 @@ const controls = [
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Price: <strong>&pound;{props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
+    <button className={classes.OrderButton} disabled={!props.purchasable}>
+      ORDER NOW
+    </button>
   </div>
 );
 
